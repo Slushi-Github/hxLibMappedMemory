5706a11df6206c2d70006c3761e4e09961ddcff7
Andres_Github <81278332+Slushi-Github@users.noreply.github.com>
2025-04-27 11:24:33 -0400

    fixes for include

--- memory/Mappedmemory.hx
+++ memory/Mappedmemory.hx
@@ -1,22 +1,28 @@
 package memory;
 
 @:cppInclude("memory/mappedmemory.h") @:include("memory/mappedmemory.h")
-extern class Mappedmemory {
+extern class MappedMemory {
 	@:native("MemoryMappingEffectiveToPhysical")
+	@:include("memory/mappedmemory.h")
 	extern public static function MemoryMappingEffectiveToPhysical(address:Int):Int;
 
 	@:native("MemoryMappingPhysicalToEffective")
+	@:include("memory/mappedmemory.h")
 	extern public static function MemoryMappingPhysicalToEffective(address:Int):Int;
 
 	@:native("MEMAllocFromMappedMemory")
+	@:include("memory/mappedmemory.h")
 	extern public static function MEMAllocFromMappedMemory(size:Int):Ptr<Void>;
 
 	@:native("MEMAllocFromMappedMemoryEx")
+	@:include("memory/mappedmemory.h")
 	extern public static function MEMAllocFromMappedMemoryEx(size:Int, alignment:Int):Ptr<Void>;
 
 	@:native("MEMAllocFromMappedMemoryForGX2Ex")
+	@:include("memory/mappedmemory.h")
 	extern public static function MEMAllocFromMappedMemoryForGX2Ex(size:Int, alignment:Int):Ptr<Void>;
 
 	@:native("MEMFreeToMappedMemory")
+	@:include("memory/mappedmemory.h")
 	extern public static function MEMFreeToMappedMemory(ptr:Ptr<Void>):Void;
 }
